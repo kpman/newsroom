eb1a3ca744f42d63ec0488413eb94c64b0148345
kpman <s92f002@hotmail.com>
2017-11-17 22:35:25 +0800

    Add esc event to exit

--- src/cli.js
+++ src/cli.js
@@ -5,3 +5,11 @@ const main = require('./main');
 main(minimist(process.argv.slice(2)))
   .then(console.log)
   .catch(console.error);
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', chunk => {
+  if (chunk === '\u001b') {
+    // ESC
+    process.exit(0);
+  }
+});

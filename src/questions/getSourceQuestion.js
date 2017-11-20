e181f5150f8caee1ea9b34cba032c23adcc5857b
Daniel Tseng <s92f002@hotmail.com>
2017-11-21 00:14:31 +0800

    Adjust autocomplete usage to catch enter event. (#20)

--- src/questions/getSourceQuestion.js
+++ src/questions/getSourceQuestion.js
@@ -3,8 +3,6 @@ const searchSources = require('./searchSources');
 module.exports = sources => ({
   type: 'autocomplete',
   name: 'source',
-  suggestOnly: true,
   message: 'Please choose a source:',
   source: searchSources(sources),
-  validate: val => (val ? true : 'Type something!'),
 });
